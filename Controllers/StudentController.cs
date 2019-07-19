83983a0a0299fce1960a93531642157cf0ec03ac
Jalpesh Vadgama <jalpesh.vadgama@gmail.com>
2019-07-19 22:24:39 +0530

    Added the changes for the student changes

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 using CoolApi.Models;
+using System.Threading.Tasks;
 
 namespace CoolApi.Controllers
 {
@@ -21,6 +22,38 @@ namespace CoolApi.Controllers
         public ActionResult<IEnumerable<Student>> Get()
         {
             return _studentContext.Students.ToList();
+        }
+
+        [HttpGet]
+        [Route("getbyid/{id}")]
+        public ActionResult<Student> GetById(int id)
+        {
+            if (id <= 0)
+            {
+                return NotFound("Student id must be higher than zero");
+            }
+            Student student = _studentContext.Students.FirstOrDefault(s => s.StudentId == id);
+            if (student == null)
+            {
+                return NotFound("Student not found");
+            }
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody]Student student)
+        {
+            if (student == null)
+            {
+                return NotFound("Student data is not supplied");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            await _studentContext.Students.AddAsync(student);
+            await _studentContext.SaveChangesAsync();
+            return Ok(student);
         }
 
         ~StudentController()
--- Models/Student.cs
+++ Models/Student.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CoolApi.Models
 {
     public class Student
     {
         public int StudentId { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+
         public string City { get; set; }
         public string State { get; set; }
     }
